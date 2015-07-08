6d2f43b85d7625d1df7c057ab7f4828904e6ad8b
jphoffer <Jon@Jonathan-Hoffers-MacBook-Pro.local>
2015-07-07 21:44:57 -0700

    MVC Exercise and Card Flip start

--- card-match/js/flip.js
+++ card-match/js/flip.js
@@ -1,5 +1,9 @@
-//cards randomize and shuffle 
+//cards randomize and shuffle
 //when clicked card flips and stays flipped
 //if cards match (=) they stay
 //if cards are not = the flip over
 //when all cards are flipped h1 changes
+
+var cardFlip = (){
+
+};
