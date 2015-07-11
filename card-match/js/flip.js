029796e6778c54b4e6e5751f270c3941dba559c0
jphoffer <Jon@Jonathan-Hoffers-MacBook-Pro.local>
2015-07-11 14:07:29 -0700

    all flipped

--- card-match/js/flip.js
+++ card-match/js/flip.js
@@ -5,25 +5,62 @@ var arr = [
   'heart', 'spade', 'club', 'diamond'
 ];
 
+var firstCardFlipped;
+
 var init = function(table) {
   var shuffledArray = shuffle(arr);
   shuffledArray.forEach(function(suit) {
     var card = makeCard(suit);
 
     card.addEventListener('click', function() {
-      card.classList.add('flipped');
+      flipAndCompare(card);
     });
 
     table.appendChild(card);
   });
 };
 
+var flipAndCompare = function(card) {
+  if (card.classList.contains('flipped')) {
+    return;
+  }
+
+  flip(card);
+
+  if (firstCardFlipped == null) {
+    firstCardFlipped = card;
+    return;
+  }
+
+  if (getSuit(firstCardFlipped) != getSuit(card)) {
+    flip(card);
+    setTimeout(function(){
+      unFlip(card);
+      unFlip(firstCardFlipped);
+      firstCardFlipped = null;
+    },1000);
+  } else {
+    firstCardFlipped = null;
+  }
+};
+
+var flip = function(card){
+  card.classList.add('flipped');
+}
+
+var unFlip = function(card) {
+  card.classList.remove('flipped');
+}
+
+var getSuit = function(card) {
+  return card.dataset.suit;
+}
+
 var makeCard = function(suit) {
   var card = document.createElement('div');
   var front = document.createElement('div');
   var back = document.createElement('div');
   var cardSuit = document.createElement('div');
-
 
   card.classList.add('card');
   front.classList.add('front');
@@ -33,6 +70,8 @@ var makeCard = function(suit) {
   card.appendChild(front);
   card.appendChild(back);
   front.appendChild(cardSuit);
+
+  card.dataset.suit = suit;
 
   return card;
 }
