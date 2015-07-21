c5e4d1d18513392e12f114fdf305a5f4385fe013
jphoffer <Jon@Jonathan-Hoffers-MacBook-Pro.local>
2015-07-20 17:36:48 -0700

    Added bio

---
+++ final_project/js/poem.js
@@ -0,0 +1,28 @@
+var arr = ['monkey', 'banana', 'brain', 'zombie', 'prom']
+shuffle(arr);
+console.log(arr);
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var poem = document.getElementById('poem-text');
+
+arr.appendChild(poem);
+
+init();
