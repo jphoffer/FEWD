8c0a01aa83f533664e21a1b8d40e717c1569239f
jphoffer <Jon@Jonathan-Hoffers-MacBook-Pro.local>
2015-07-25 13:51:51 -0700

    poem works. need to shuffle title

--- final_project/js/poem.js
+++ final_project/js/poem.js
@@ -1,14 +1,20 @@
-var arr = ['monkey', '</br>', '</br>', 'banana', 'brain', 'wander', 'cell', 'prom', 'fatal', 'mourning']
+var arr = ['</br>', '</br>', '<p>The entire history of human desire takes about seventy minutes to tell.</p><p>Unfortunately, we don’t have that kind of time.</p>','How do I love thee? Let me count the ways', 'Shall I compare thee to a summers day', 'Once upon a midnight dreary, while I pondered weak and weary', '<p>But I have promises to keep,</p><p>And miles to go before I sleep,</p>', 'In Xanadu did Kubla Khan', 'Do not go gentle into that good night', 'A narrow fellow in the grass', '<p>Because I could not stop for death</p><p>He kindly stopped for me</p>', 'I wandered lonely as a cloud', 'At twenty I tried to die', 'And they stuck me together with glue.', '<p>Into the mouth of Hell</p><p>Rode the six hundred.</p>', 'I saw the best minds of my generation destroyed by madness, starving hysterical naked,', 'And that has made all the difference.', 'and this is the wonder that’s keeping the stars apart', 'O Captain! my Captain! our fearful trip is done', 'Of Mans first disobedience, and the fruit', 'Of dog eat dog, of mighty crush the weak.', 'Blood on the leaves and blood at the root,', 'You smug-faced crowds with kindling eye', 'You will be alone always and then you will die.', 'Get out as early as you can,', 'Then you see whats written on his hard drive', '<p>They dont make it</p><p>the beautiful die in flame- </p><p>sucide pills, rat poison, rope what</p><p>ever.</p>', '</br>', '</br>', '</br>', '</br>', '</br>', '</br>']
 
+var titleArr = ['words', 'rules', 'facsism', 'party monster']
 
 var writePoem = function(){
   var poem = document.getElementById('poem-text');
   var words = shuffle(arr);
   poem.innerHTML = words.join(' ');
-
+  writeTitle();
 
 };
 
+var writeTitle = function(){
+  var title = document.getElementById('poem-title');
+  var titleWords = titleArr;
+  title.innerHTML = titleWords.join(' ');
+}
 
 
  var btn = document.getElementById('btn');
